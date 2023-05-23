f9eb9f074b95ef57fb3aebeee1e17084e07ecc5d
Ramiro <rrcolumbia21@gmail.com>
2023-05-23 07:54:44 +0100

    parts

---
+++ index.js
@@ -0,0 +1,23 @@
+const WIDTH = 800;
+const HEIGHT = 256;
+const GRAVITY = 1;
+const JUMP_STRENGTH = -20;
+const JUMP_STOP_VELOCITY = -4;
+const OBSTACLE_SIZE = { width: 32, height: 64 };
+const LOCALSTORAGE_HIGHSCORE_KEY = "boing-highscore";
+const UPDATE_INTERVAL_MS = 1000 / 60;
+
+let updateInterval = null;
+
+const CONTROLS = {
+    jump: [" ", "w", "arrowup"],
+    pause: ["p", "escape"],
+    reset: ["r"],
+};
+
+const BOING_EL = document.querySelector("#boing");
+const OBSTACLES_EL = document.querySelector("#obstacles");
+const MESSAGE_EL = document.querySelector("#message");
+const SCORE_EL = document.querySelector("#score");
+const HIGHSCORE_MESSAGE_EL =
+    document.querySelector("#highscore");
