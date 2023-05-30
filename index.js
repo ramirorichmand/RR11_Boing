1a60cd4f019ae82eeca124550f8aa9fff39d7375
Ramiro <rrcolumbia21@gmail.com>
2023-05-30 07:39:08 +0100

    entities colliding

--- index.js
+++ index.js
@@ -412,5 +412,32 @@ function renderScore(score) {
     SCORE_EL.innerText = score;
 }
 
-// render score
+function setElementPosition(element, { x, y }) {
+    if (typeof x === "number") {
+        element.style.left = `${x}px`;
+    }
+    if (typeof y === "number") {
+        element.style.top = `${y}px`;
+    }
+}
+
+function doEntitiesCollide(entityA, entityB) {
+    const aLef = entityA.x;
+    const aRig = entityA.x + entityA.width;
+    const aTop = entityA.y;
+    const aBot = entityA.y + entityA.height;
+    const bLef = entityB.x;
+    const bRig = entityB.x + entityB.width;
+    const bTop = entityB.y;
+    const bBot = entityB.y + entityB.height;
+
+    return (
+        aLef <= bRig &&
+        aRig >= bLef &&
+        aTop <= bBot &&
+        aBot >= bTop
+    );
+}
+
+// entities colliding
 
